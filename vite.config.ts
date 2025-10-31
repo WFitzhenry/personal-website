df0a249bc63e1300e042abef82eaac98bd541d2e
WFitzhenry <warrenfitzhenry22@gmail.com>
2025-10-31 11:45:18 +0000

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    rollupOptions: {
-      input: {
-        main: '/index.html',
-        web: '/src/pages/web-projects.html',
-        mobile: '/src/pages/mobile-projects.html',
-        ml: '/src/pages/ml-projects.html',
-        other: '/src/pages/other-projects.html',
-      },
-    },
+    outDir: 'dist',
   },
 });
